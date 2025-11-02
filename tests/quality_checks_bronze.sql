f3cdd21a3ad709336708f0a55531d9818baf073f
Hadeer Elkady <134949853+hadeeralkady@users.noreply.github.com>
2025-11-02 22:42:49 +0200

    Enhance quality checks for crm_sales_details
    Added checks for invalid dates and data consistency in sales details.

--- tests/quality_checks_bronze.sql
+++ tests/quality_checks_bronze.sql
@@ -63,3 +63,54 @@ SELECT
     * 
 FROM bronze.crm_prd_info
 WHERE prd_end_dt < prd_start_dt;
+-- ====================================================================
+-- Checking 'bronze.crm_sales_details'
+-- ====================================================================
+SELECT * FROM bronze.crm_sales_details
+
+-- Check for Invalid Dates
+select 
+nullif (sls_order_dt,0) sls_order_dt
+FROM bronze.crm_sales_details
+where sls_order_dt <= 0
+
+ -- Check for Invalid Date Orders (Order Date > Shipping/Due Dates)
+SELECT
+NULLIF(sls_ship_dt,0) sls_ship_dt
+FROM bronze.crm_sales_details
+WHERE sls_ship_dt <= 0 OR LEN(sls_ship_dt) != 8 OR sls_ship_dt > 20500101 OR sls_ship_dt < 19000101
+
+SELECT
+NULLIF(sls_due_dt,0) sls_due_dt
+FROM bronze.crm_sales_details
+WHERE sls_due_dt <= 0 OR LEN(sls_due_dt) != 8 OR sls_due_dt > 20500101 OR sls_due_dt < 19000101
+
+SELECT * 
+FROM bronze.crm_sales_details
+WHERE sls_order_dt > sls_ship_dt OR sls_order_dt > sls_due_dt
+
+-- Check Data Consistency: Sales = Quantity * Price
+SELECT DISTINCT
+sls_sales,
+sls_quantity,
+sls_price
+FROM bronze.crm_sales_details
+WHERE sls_sales != sls_quantity * sls_price
+OR sls_sales IS NULL OR sls_quantity IS NULL OR sls_price IS NULL
+OR sls_sales <= 0 OR sls_quantity <= 0 OR sls_price <= 0
+ORDER BY sls_sales,sls_quantity,sls_price
+
+SELECT DISTINCT
+sls_sales AS old_sls_sales,
+sls_quantity,
+sls_price as old_sls_price,
+CASE WHEN sls_sales IS NULL OR sls_sales <= 0 OR sls_sales != sls_quantity * ABS(sls_price)
+		THEN sls_quantity * ABS(sls_price)
+	ELSE sls_sales
+
+END AS sls_sales, 
+CASE WHEN sls_price is NULL OR sls_price <= 0
+		THEN sls_sales / NULLIF(sls_quantity , 0)
+	 ELSE sls_price
+END AS sls_price
+FROM bronze.crm_sales_details
