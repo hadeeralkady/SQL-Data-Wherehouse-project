0d5a988490e4ef3b5b0e062c6e490a70a7f8fc10
Hadeer Elkady <134949853+hadeeralkady@users.noreply.github.com>
2025-11-02 03:27:23 +0200

    Add quality checks for bronze.crm_prd_info
    Added quality checks for 'bronze.crm_prd_info' including checks for nulls, duplicates, unwanted spaces, negative values, data standardization, and invalid date orders.

--- tests/quality_checks_bronze.sql
+++ tests/quality_checks_bronze.sql
@@ -1,7 +1,9 @@
 USE DataWherehouse;
 
 SELECT * FROM bronze.crm_cust_info;
-
+-- ====================================================================
+-- Checking 'bronze.crm_cust_info'
+-- ====================================================================
 -- Check For Nulls or Duplicates in Primary Key
 -- Expectation: No Result
 SELECT 
@@ -28,3 +30,36 @@ FROM bronze.crm_cust_info;
 SELECT DISTINCT 
     cst_marital_status
 FROM bronze.crm_cust_info;
+
+-- ====================================================================
+-- Checking 'bronze.crm_prd_info'
+-- ====================================================================
+-- Check For Nulls or Duplicates in Primary Key
+SELECT 
+    prd_id,
+    COUNT(*) 
+FROM bronze.crm_prd_info
+GROUP BY prd_id
+HAVING COUNT(*) > 1 OR prd_id IS NULL;
+
+--Check for unwanted Spaces
+SELECT prd_nm
+FROM bronze.crm_prd_info
+WHERE prd_nm != TRIM(prd_nm);
+
+-- Check for NULLs or Negative Values in Cost
+SELECT 
+    prd_cost 
+FROM bronze.crm_prd_info
+WHERE prd_cost < 0 OR prd_cost IS NULL;
+
+-- Data Standardization & Consistency
+SELECT DISTINCT 
+    prd_line 
+FROM bronze.crm_prd_info;
+
+-- Check for Invalid Date Orders (Start Date > End Date)
+SELECT 
+    * 
+FROM bronze.crm_prd_info
+WHERE prd_end_dt < prd_start_dt;
