ad8e01ecc1031a3104736ce7b7989e562cab33d0
Hadeer Elkady <134949853+hadeeralkady@users.noreply.github.com>
2025-11-02 03:19:44 +0200

    Add quality checks for silver.crm_prd_info
    Added quality checks for 'silver.crm_prd_info' including checks for NULLs, duplicates, unwanted spaces, negative values, and date validity.

--- tests/quality_checks_silver.sql
+++ tests/quality_checks_silver.sql
@@ -46,3 +46,37 @@ SELECT DISTINCT
     cst_marital_status
 FROM silver.crm_cust_info;
 SELECT * FROM silver.crm_cust_info;
+
+-- ====================================================================
+-- Checking 'silver.crm_prd_info'
+-- ====================================================================
+-- Check for NULLs or Duplicates in Primary Key
+SELECT 
+    prd_id,
+    COUNT(*) 
+FROM silver.crm_prd_info
+GROUP BY prd_id
+HAVING COUNT(*) > 1 OR prd_id IS NULL;
+
+-- Check for Unwanted Spaces
+SELECT 
+    prd_nm 
+FROM silver.crm_prd_info
+WHERE prd_nm != TRIM(prd_nm);
+
+-- Check for NULLs or Negative Values in Cost
+SELECT 
+    prd_cost 
+FROM silver.crm_prd_info
+WHERE prd_cost < 0 OR prd_cost IS NULL;
+
+-- Data Standardization & Consistency
+SELECT DISTINCT 
+    prd_line 
+FROM silver.crm_prd_info;
+
+-- Check for Invalid Date Orders (Start Date > End Date)
+SELECT 
+    * 
+FROM silver.crm_prd_info
+WHERE prd_end_dt < prd_start_dt;
